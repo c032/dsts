a0de329d67e4ec183f9ccee3bb5bfecb442361df
c032 <git@c032.dev>
2024-02-10 23:32:33 +0000

    notifier: Add comments

--- notifier.go
+++ notifier.go
@@ -1,11 +1,16 @@
 package dsts
 
+// Notifier can run callbacks when there's a new update on some data, so that
+// the i3 status line can be redrawn.
 type Notifier interface {
+	// OnUpdate registers `callback` to be called when there's an update.
+	//
+	// It returns a function that, when called, should un-register `callback`
+	// so that it's no longer called on updates.
 	OnUpdate(callback OnUpdateCallbackFunc) RemoveOnUpdateCallbackFunc
 }
 
 type OnUpdateCallbackFunc func()
-
 type RemoveOnUpdateCallbackFunc func()
 
 type NotifierFunc func(callback OnUpdateCallbackFunc) RemoveOnUpdateCallbackFunc
